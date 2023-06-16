db9a3e3b78fdc73f1bbee51903914b5edcf4adfb
doehyoungkim <132883939+doehyoungkim@users.noreply.github.com>
2023-06-16 14:14:55 +0900

    Add files via upload

---
+++ Bulletin/src/main/java/Controller/PostsApiController.java
@@ -0,0 +1,14 @@
+package Controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/posts")
+public class PostsApiController {
+    @PostMapping
+    
+
+}
